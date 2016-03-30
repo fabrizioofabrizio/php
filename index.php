2fc1926134280c2f88298c02a26c65c098d01787
fabrizioofabrizio <applicazionepercellulare@gmail.com>
2016-03-30 12:13:17 +0200

    Update index.php

--- index.php
+++ index.php
@@ -8,10 +8,10 @@
 //////////////////////////////////////////////////
 //////////////////////////////////////////////////
 
-$serverName = "mysql.hostinger.it";
-$userName = "u149252705_users";
-$password = "23y54bak";
-$dbName = "u149252705_test";
+$serverName = "";
+$userName = "";
+$password = "";
+$dbName = "";
 /*
 $serverName = "localhost";
 $userName = "root";
